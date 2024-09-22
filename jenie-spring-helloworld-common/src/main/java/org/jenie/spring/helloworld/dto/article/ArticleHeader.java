e6811df62b92f936541dfb0dc7bf5994193c6335
thinkhard.j.park <thinkhard.j.park@gmail.com>
2024-09-22 10:51:36 +0900

    MongoDBConfig에 Zonedatetime convert 버그 수정

--- jenie-spring-helloworld-common/src/main/java/org/jenie/spring/helloworld/dto/article/ArticleHeader.java
+++ jenie-spring-helloworld-common/src/main/java/org/jenie/spring/helloworld/dto/article/ArticleHeader.java
@@ -1,8 +1,10 @@
 package org.jenie.spring.helloworld.dto.article;
 
 import org.jenie.spring.helloworld.dto.board.Board;
+import org.jenie.spring.helloworld.pojo.ActionDateTime;
 import org.jenie.spring.helloworld.pojo.Reaction;
 import org.jenie.spring.helloworld.pojo.Writer;
 
-public record ArticleHeader(String id, Board board, String title, Reaction reaction, Writer writer) {
+public record ArticleHeader(String id, Board board, String title, Reaction reaction, Writer writer,
+		ActionDateTime actionDateTime) {
 }
