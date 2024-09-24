df0080c5051ad6c4333c3cbae88a608e690de4ad
thinkhard.j.park <thinkhard.j.park@gmail.com>
2024-09-24 21:22:13 +0900

    Add boardId check

--- jenie-spring-helloworld/src/main/java/org/jenie/spring/helloworld/mapper/ArticleHeaderMapper.java
+++ jenie-spring-helloworld/src/main/java/org/jenie/spring/helloworld/mapper/ArticleHeaderMapper.java
@@ -2,7 +2,9 @@ package org.jenie.spring.helloworld.mapper;
 
 import org.jenie.spring.helloworld.dto.article.ArticleHeader;
 import org.jenie.spring.helloworld.entity.article.ArticleHeaderEntity;
+import org.jenie.spring.helloworld.entity.board.BoardEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -11,6 +13,10 @@ public interface ArticleHeaderMapper {
 	ArticleHeaderMapper INSTANCE = Mappers.getMapper(ArticleHeaderMapper.class);
 
 	ArticleHeader toDto(ArticleHeaderEntity entity);
+
+	@Mapping(target = "board", source = "board")
+	@Mapping(target = ".", source = "articleHeader")
+	ArticleHeader toDto(ArticleHeaderEntity articleHeader, BoardEntity board);
 
 	ArticleHeaderEntity toEntity(ArticleHeader dto);
 
