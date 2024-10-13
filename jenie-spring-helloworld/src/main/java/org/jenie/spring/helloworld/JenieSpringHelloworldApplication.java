8951585c87c2b386202abbeb3071c7fb03ae812d
thinkhard.j.park <thinkhard.j.park@gmail.com>
2024-10-13 23:16:28 +0900

    #8: jacoco exclude code coverage
    - configuration
    - private method of utility class
    - toString, etc.

--- jenie-spring-helloworld/src/main/java/org/jenie/spring/helloworld/JenieSpringHelloworldApplication.java
+++ jenie-spring-helloworld/src/main/java/org/jenie/spring/helloworld/JenieSpringHelloworldApplication.java
@@ -1,10 +1,13 @@
 package org.jenie.spring.helloworld;
 
+import org.jenie.spring.helloworld.common.ExcludeCodeCoverageGenerated;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
+@ExcludeCodeCoverageGenerated
 @SpringBootApplication(exclude = { MongoAutoConfiguration.class, MongoDataAutoConfiguration.class })
 public class JenieSpringHelloworldApplication {
 
