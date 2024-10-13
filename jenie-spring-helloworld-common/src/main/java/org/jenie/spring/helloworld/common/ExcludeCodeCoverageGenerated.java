ae643eae060a9a3b1e419fa9bc10402251147c34
thinkhard.j.park <thinkhard.j.park@gmail.com>
2024-10-13 23:11:17 +0900

    #8: jacoco exclude test5 - annotation based

--- jenie-spring-helloworld-common/src/main/java/org/jenie/spring/helloworld/common/ExcludeCodeCoverageGenerated.java
+++ jenie-spring-helloworld-common/src/main/java/org/jenie/spring/helloworld/common/ExcludeCodeCoverageGenerated.java
@@ -6,6 +6,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * <a href="https://github.com/jacoco/jacoco/wiki/FilteringOptions">Annotation-Based
+ * Filtering</a> 을 참고한다. 어노테이션 이름에 Generated 가 포함되어야 한다.
+ *
+ * @author thinkhardj park
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR })
