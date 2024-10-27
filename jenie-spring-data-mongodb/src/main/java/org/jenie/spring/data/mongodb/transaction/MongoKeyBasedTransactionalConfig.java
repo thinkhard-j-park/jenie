5b1fbe7c0be554eaaebe8250879fe033a7e78628
thinkhard.j.park <thinkhard.j.park@gmail.com>
2024-10-27 17:42:27 +0900

    #8: exclude code coverage MongoKeyBasedTransactionalConfig

--- jenie-spring-data-mongodb/src/main/java/org/jenie/spring/data/mongodb/transaction/MongoKeyBasedTransactionalConfig.java
+++ jenie-spring-data-mongodb/src/main/java/org/jenie/spring/data/mongodb/transaction/MongoKeyBasedTransactionalConfig.java
@@ -1,11 +1,13 @@
 package org.jenie.spring.data.mongodb.transaction;
 
 import org.jenie.spring.data.mongodb.operation.MongoTemplateRouterConfig;
+import org.jenie.spring.util.ExcludeCodeCoverageGenerated;
 
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+@ExcludeCodeCoverageGenerated
 @Configuration
 @AutoConfigureAfter(MongoTemplateRouterConfig.class)
 @ComponentScan(basePackageClasses = MongoKeyBasedTransactionAspect.class)
