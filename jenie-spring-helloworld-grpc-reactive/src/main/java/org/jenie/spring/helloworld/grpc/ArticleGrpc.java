5c4a31456587b9212a94aa5abe8bbec6fe5be25c
thinkhard.j.park <thinkhard.j.park@gmail.com>
2025-04-01 09:02:32 +0900

    #44: Implement reactive grpc

--- jenie-spring-helloworld-grpc-reactive/src/main/java/org/jenie/spring/helloworld/grpc/ArticleGrpc.java
+++ jenie-spring-helloworld-grpc-reactive/src/main/java/org/jenie/spring/helloworld/grpc/ArticleGrpc.java
@@ -1,8 +1,10 @@
 package org.jenie.spring.helloworld.grpc;
 
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.jenie.spring.helloworld.dto.article.ArticleHeader;
 import org.jenie.spring.helloworld.dto.article.ArticleHeaderList;
 import org.jenie.spring.helloworld.dto.article.ArticleHeaderListMessage;
+import org.jenie.spring.helloworld.dto.article.ArticleHeaderMessage;
 import org.jenie.spring.helloworld.dto.article.ListArticleHeaderRequestParam;
 import org.jenie.spring.helloworld.reactive.service.ReactiveArticleService;
 import reactor.core.publisher.Mono;
@@ -14,6 +16,12 @@ public class ArticleGrpc extends ReactorArticleServiceGrpc.ArticleServiceImplBas
 
 	public ArticleGrpc(ReactiveArticleService articleService) {
 		this.articleService = articleService;
+	}
+
+	@Override
+	public Mono<ArticleHeaderMessage> getArticleHeaderById(GetArticleHeaderByIdRequestMessage request) {
+		return this.articleService.getArticleHeaderById(request.getService(), request.getId(), request.getLatest())
+			.map(ArticleHeader::toProtoMessage);
 	}
 
 	@Override
