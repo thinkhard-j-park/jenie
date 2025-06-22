21ef736c7dd8bb8df60066f33698636738471eeb
thinkhard.j.park <thinkhard.j.park@gmail.com>
2025-06-22 14:16:37 +0900

    Revert "Remove redundant MongoDB auto-configuration exclusions and centralize configuration."
    This reverts commit e3621fc3879e8776cce43c9c3fd679a73a3a8779.

--- jenie-spring-helloworld-rest/src/main/java/org/jenie/spring/helloworld/JenieSpringHelloworldRestApplication.java
+++ jenie-spring-helloworld-rest/src/main/java/org/jenie/spring/helloworld/JenieSpringHelloworldRestApplication.java
@@ -2,8 +2,10 @@ package org.jenie.spring.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { MongoAutoConfiguration.class, MongoDataAutoConfiguration.class })
 public class JenieSpringHelloworldRestApplication {
 
 	public static void main(String[] args) {
