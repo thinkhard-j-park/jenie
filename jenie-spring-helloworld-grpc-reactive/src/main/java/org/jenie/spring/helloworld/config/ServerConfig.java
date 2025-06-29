83a5ca47520edc4a1d148de0eff71140ccf65e8d
thinkhard.j.park <thinkhard.j.park@gmail.com>
2025-06-29 21:23:15 +0900

    Add conditional access log configuration support for gRPC servers and enhance MongoDB pool properties

--- jenie-spring-helloworld-grpc-reactive/src/main/java/org/jenie/spring/helloworld/config/ServerConfig.java
+++ jenie-spring-helloworld-grpc-reactive/src/main/java/org/jenie/spring/helloworld/config/ServerConfig.java
@@ -3,6 +3,7 @@ package org.jenie.spring.helloworld.config;
 import io.grpc.ServerInterceptor;
 import org.jenie.spring.helloworld.log.access.SpringGrpcAccessLogInterceptor;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -11,11 +12,17 @@ import org.springframework.grpc.server.GlobalServerInterceptor;
 @Configuration
 public class ServerConfig {
 
-	@Bean
-	@Order(100)
-	@GlobalServerInterceptor
-	ServerInterceptor accessLogInterceptor() {
-		return new SpringGrpcAccessLogInterceptor();
+	@ConditionalOnProperty(name = { "helloworld.enable-access-log" }, havingValue = "true")
+	@Configuration
+	public static class AccessLogConfig {
+
+		@Bean
+		@Order(100)
+		@GlobalServerInterceptor
+		ServerInterceptor accessLogInterceptor() {
+			return new SpringGrpcAccessLogInterceptor();
+		}
+
 	}
 
 }
