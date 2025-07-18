be06c0059c0ed9c55f015a76bc83dfbd4d972cc3
thinkhard.j.park <thinkhard.j.park@gmail.com>
2025-07-18 10:49:45 +0900

    Refactor `ReactiveBoardService` to use `AsyncLoadingCache` for improved asynchronous caching support and CompletableFuture integration.

--- jenie-spring-helloworld-web/src/main/java/org/jenie/spring/helloworld/reactive/service/ReactiveBoardService.java
+++ jenie-spring-helloworld-web/src/main/java/org/jenie/spring/helloworld/reactive/service/ReactiveBoardService.java
@@ -1,10 +1,12 @@
 package org.jenie.spring.helloworld.reactive.service;
 
 import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
-import com.github.benmanes.caffeine.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.AsyncCacheLoader;
+import com.github.benmanes.caffeine.cache.AsyncLoadingCache;
 import com.github.benmanes.caffeine.cache.Caffeine;
-import com.github.benmanes.caffeine.cache.LoadingCache;
 import org.jenie.spring.helloworld.annotation.ConditionalOnReactive;
 import org.jenie.spring.helloworld.entity.board.BoardEntity;
 import org.jenie.spring.helloworld.reactive.repository.ReactiveBoardRepository;
@@ -12,7 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 
-import org.springframework.lang.Nullable;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 @ConditionalOnReactive
@@ -23,19 +25,19 @@ public class ReactiveBoardService {
 
 	private static final int TTL_MINUTES = 10;
 
-	private final LoadingCache<BoardKey, Mono<BoardEntity>> boardCache;
+	private final AsyncLoadingCache<BoardKey, BoardEntity> boardCache;
 
 	public ReactiveBoardService(ReactiveBoardRepository boardRepository) {
 		this.boardCache = Caffeine.newBuilder()
 			.expireAfterWrite(Duration.ofMinutes(TTL_MINUTES))
-			.build(new BoardLoader(boardRepository));
+			.buildAsync(new BoardLoader(boardRepository));
 	}
 
 	public Mono<BoardEntity> findBoardEntityById(String service, String id) {
-		return this.boardCache.get(new BoardKey(service, id));
+		return Mono.fromFuture(this.boardCache.get(new BoardKey(service, id)));
 	}
 
-	static class BoardLoader implements CacheLoader<BoardKey, Mono<BoardEntity>> {
+	static class BoardLoader implements AsyncCacheLoader<BoardKey, BoardEntity> {
 
 		private final ReactiveBoardRepository boardRepository;
 
@@ -44,8 +46,9 @@ public class ReactiveBoardService {
 		}
 
 		@Override
-		public @Nullable Mono<BoardEntity> load(BoardKey boardKey) {
-			return this.boardRepository.findBoardById(boardKey.dbKey(), boardKey.id()).cache();
+		public @NonNull CompletableFuture<? extends BoardEntity> asyncLoad(@NonNull BoardKey boardKey,
+				@NonNull Executor executor) {
+			return this.boardRepository.findBoardById(boardKey.dbKey(), boardKey.id()).toFuture();
 		}
 
 	}
