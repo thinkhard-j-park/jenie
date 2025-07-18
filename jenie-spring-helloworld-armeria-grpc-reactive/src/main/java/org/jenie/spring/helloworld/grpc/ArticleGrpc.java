e77b07bc80dabfb8aa2daec7ebb4b935629fbf76
thinkhard.j.park <thinkhard.j.park@gmail.com>
2025-07-18 17:07:12 +0900

    Remove `Schedulers.parallel()` from gRPC service methods for simpler reactive pipeline execution.

--- jenie-spring-helloworld-armeria-grpc-reactive/src/main/java/org/jenie/spring/helloworld/grpc/ArticleGrpc.java
+++ jenie-spring-helloworld-armeria-grpc-reactive/src/main/java/org/jenie/spring/helloworld/grpc/ArticleGrpc.java
@@ -13,7 +13,6 @@ import org.jenie.spring.helloworld.dto.article.ArticleRequest;
 import org.jenie.spring.helloworld.dto.article.ListArticleHeaderRequestParam;
 import org.jenie.spring.helloworld.reactive.service.ReactiveArticleService;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
 import org.springframework.stereotype.Service;
 
@@ -29,14 +28,12 @@ public class ArticleGrpc extends ReactorArticleServiceGrpc.ArticleServiceImplBas
 	@Override
 	public Mono<ArticleHeaderMessage> getArticleHeaderById(GetArticleHeaderByIdRequestMessage request) {
 		return this.articleService.getArticleHeaderById(request.getService(), request.getId(), request.getLatest())
-			.publishOn(Schedulers.parallel())
 			.map(ArticleHeader::toProtoMessage);
 	}
 
 	@Override
 	public Mono<ArticleMessage> viewArticle(ViewArticleRequestMessage request) {
 		return this.articleService.viewArticle(request.getService(), request.getId(), request.getIncViewCount())
-			.publishOn(Schedulers.parallel())
 			.map(Article::toProtoMessage);
 	}
 
@@ -45,9 +42,7 @@ public class ArticleGrpc extends ReactorArticleServiceGrpc.ArticleServiceImplBas
 		var service = request.getService();
 		var param = new ListArticleHeaderRequestParam(request.getBoardId(), request.getPrevArticleId(),
 				request.getSize(), request.getSort());
-		return this.articleService.listArticleHeader(service, param)
-			.publishOn(Schedulers.parallel())
-			.map(ArticleHeaderList::toProtoMessage);
+		return this.articleService.listArticleHeader(service, param).map(ArticleHeaderList::toProtoMessage);
 	}
 
 	@Override
@@ -55,9 +50,7 @@ public class ArticleGrpc extends ReactorArticleServiceGrpc.ArticleServiceImplBas
 		var articleRequest = new ArticleRequest(request.getBoardId(), request.getTitle(), request.getContent(),
 				Writer.fromProtoMessage(request.getWriter()));
 
-		return this.articleService.writeArticle(request.getService(), articleRequest)
-			.publishOn(Schedulers.parallel())
-			.map(Article::toProtoMessage);
+		return this.articleService.writeArticle(request.getService(), articleRequest).map(Article::toProtoMessage);
 	}
 
 	@Override
@@ -66,14 +59,12 @@ public class ArticleGrpc extends ReactorArticleServiceGrpc.ArticleServiceImplBas
 				Writer.fromProtoMessage(request.getWriter()));
 
 		return this.articleService.modifyArticle(request.getService(), request.getId(), articleRequest)
-			.publishOn(Schedulers.parallel())
 			.map(Article::toProtoMessage);
 	}
 
 	@Override
 	public Mono<ArticleDeleteResultMessage> deleteArticle(DeleteArticleRequestMessage request) {
 		return this.articleService.deleteArticle(request.getService(), request.getId())
-			.publishOn(Schedulers.parallel())
 			.map(ArticleDeleteResult::toProtoMessage);
 	}
 
